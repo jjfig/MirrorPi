c686aa23356669b57af596dc6a31951bb20a72de
Julian Figueroa <jjfig@JFIGG-MB-Air.attlocal.net>
2015-12-28 14:41:18 -0800

    initial commit

---
+++ js/config.js
@@ -0,0 +1,43 @@
+var config = {
+    lang: 'en',
+    time: {
+        timeFormat: 12
+    },
+    weather: {
+        //change weather params here:
+        //units: metric or imperial
+        params: {
+            q: 'Baarn,Netherlands',
+            units: 'imperial',
+            // if you want a different lang for the weather that what is set above, change it here
+            lang: 'en',
+            APPID: 'ca87d7f52117bb4c4cf0bf2726a78bca'
+        }
+    },
+    compliments: {
+        interval: 30000,
+        fadeInterval: 4000,
+        morning: [
+            'Good morning, handsome!',
+            'Enjoy your day!',
+            'How was your sleep?'
+        ],
+        afternoon: [
+            'Hello, beauty!',
+            'You look sexy!',
+            'Looking good today!'
+        ],
+        evening: [
+            'Wow, you look hot!',
+            'You look nice!',
+            'Hi, sexy!'
+        ]
+    },
+    calendar: {
+        maximumEntries: 10,
+        url: "https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M"
+    },
+    news: {
+        feed: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+    }
+}
